faf1607db1c69b3a28df1f9f8376d58a2fa7216a
Sergio Carracedo <info@sergiocarracedo.es>
2018-11-12 19:06:09 +0100

    process.env.DEPLOY_ENV

--- nuxt.config.js
+++ nuxt.config.js
@@ -27,7 +27,7 @@ module.exports = {
   ** Global CSS
   */
   css: [
-    process.DEPLOY_ENV == 'gh-pages' ? 'sireno-grid/dist/sireno-grid.css' : 'assets/sireno-grid/sireno-grid.scss',
+    process.env.DEPLOY_ENV == 'gh-pages' ? 'sireno-grid/dist/sireno-grid.css' : 'assets/sireno-grid/sireno-grid.scss',
     'assets/scss/sireno-web.scss'
   ],
 
