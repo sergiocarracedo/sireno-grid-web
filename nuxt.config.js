1501648e70b5861f80c7927e6d1e9e84c971fc52
Sergio Carracedo Martinez <hi@sergiocarracedo.es>
2024-12-18 23:45:54 +0100

    Update base domain

--- nuxt.config.js
+++ nuxt.config.js
@@ -30,7 +30,7 @@ module.exports = {
       {
         hid: "og:image",
         property: "og:image",
-        content: "https://sirenogrid.com/og-image.jpg"
+        content: "https://sergiocarracedo.github.io/sireno-grid-web/og-image.jpg"
       },
       {
         property: "twitter:card",
